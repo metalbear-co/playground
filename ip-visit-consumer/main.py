b9560809a76bd1002d6c058f2634a6727a2fcfe8
Aviram Hassan <aviramyhassan@gmail.com>
2025-04-24 19:04:15 +0300

    .darn

--- ip-visit-consumer/main.py
+++ ip-visit-consumer/main.py
@@ -20,7 +20,6 @@ def start_kafka_reader(address, topic, group):
     consumer = Consumer({
         'bootstrap.servers': address,
         'group.id': group,
-        'max.poll.records': 1,
         'fetch.min.bytes': 1,
         'fetch.max.wait.ms': 10,
         "debug": os.getenv("KAFKA_DEBUG")
