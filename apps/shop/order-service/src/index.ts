c0f784823b00e1aa508ebfa57dbf74325bc82402
aris-cub <aris@metalbear.com>
2026-04-09 10:27:57 +0300

    commit

--- apps/shop/order-service/src/index.ts
+++ apps/shop/order-service/src/index.ts
@@ -223,7 +223,7 @@ async function createOrderDirect(
     baggage,
   });
 
-  return { orderId, status: "confirmed" };
+  return { orderId, status: "confirmed smart" };
 }
 
 app.post("/orders", async (req, res) => {
