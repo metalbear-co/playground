e5b090eab329d80f2da9a37881a9708a00d57d73
aris-cub <aris@metalbear.com>
2026-04-19 15:36:33 +0300

    sone

--- apps/shop/order-service/src/index.ts
+++ apps/shop/order-service/src/index.ts
@@ -239,7 +239,7 @@ async function createOrderDirect(
     baggage,
   });
 
-  return { orderId, status: "confirmed" };
+  return { orderId, status: "confirmed sone" };
 }
 
 app.post("/orders", async (req, res) => {
